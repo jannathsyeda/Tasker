dc6d30d990349f5329534ac5909850ea1ca64574
jannathsyeda <jannathsyeda7@gmail.com>
2025-05-23 12:16:40 +0600

    add items done

--- tasker-project/src/Task/TaskAction.jsx
+++ tasker-project/src/Task/TaskAction.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function TaskAction({addHandleTask}) {
+export default function TaskAction({addHandleTaskModal}) {
   return (
     <div className="mb-14 items-center justify-between sm:flex">
     <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
     <div className="flex items-center space-x-5">
-      <button onClick={addHandleTask} className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold" >
+      <button onClick={addHandleTaskModal} className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold" >
         Add Task
       </button>
       <button className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold">
--- tasker-project/src/Task/TaskBoard.jsx
+++ tasker-project/src/Task/TaskBoard.jsx
@@ -15,11 +15,14 @@ export default function TaskBoard() {
     }
 
     const [ tasks , setTasks ]=useState([defaultTasks])
+    console.log(tasks)
     const [ showModal , setShowModal]=useState(false);
 
-    function addHandleTask(){
+    function addHandleTask(newTask){
 
-      console.log("adding")
+      console.log("adding task:", newTask)
+      setTasks([...tasks, newTask])
+      setShowModal(false)
     }
 
   return (
@@ -29,9 +32,9 @@ export default function TaskBoard() {
           <SearchBar />
         </div>
 
-{showModal && <AddModal/> }
+{showModal && <AddModal onSave={addHandleTask}/> }
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction addHandleTask={()=>setShowModal(true)}/>
+          <TaskAction addHandleTaskModal={()=>setShowModal(true)}/>
 
           <TaskList tasks={tasks}/>
 
